cffcdec004f1d701e8b7cc27473e21fcf2d676a6
bguilas <bryanjguilas@gmail.com>
2020-11-29 11:19:44 -0800

    added material UI table

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,24 +1,50 @@
 import React from "react";
 import "./style.css";
+import {makeStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function employee (props) {
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+export default function Employee(props) {
+    const classes = useStyles();
+
     return (
         <div>
-            {props.employeeList.length ? (
-                <ul className="list-group">
-
-                    {props.employeeList.map(result => (
-                        <li className="list-group-item" key={result.id}>
-                            <div id="name"><b>{result.firstName} {result.lastName}</b> ({result.genre})</div>
-                            <div id="other"><i>Contact:</i> {result.email} </div>
-                        </li>
-                    ))}
-                </ul >
-            ) : (
-                <h2>Employee does not exist in the database</h2>
-            )}
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Last Name</TableCell>
+                            <TableCell align="right">First Name</TableCell>
+                            <TableCell align="right">Genre</TableCell>
+                            <TableCell align="right">Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.employeeList.map((result) => (
+                            <TableRow key={result.id}>
+                                <TableCell component="th" scope="row">
+                                    {result.lastName}
+                                </TableCell>
+                                <TableCell align="right">{result.firstName}</TableCell>
+                                <TableCell align="right">{result.genre}</TableCell>
+                                <TableCell align="right">{result.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
-    )
+    );
 }
-
-export default employee;
---
+++ src/components/pasted.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+return (
+    <div>
+
+        {props.employeeList.length ? (
+            <ul className="list-group">
+
+                {props.employeeList.map(result => (
+                    <li className="list-group-item" key={result.id}>
+                        <div id="name"><b>{result.firstName} {result.lastName}</b> ({result.genre})</div>
+                        <div id="other"><i>Contact:</i> {result.email} </div>
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <h2>Employee does not exist in the database</h2>
+        )}
+    </div>
