7cc5734680931cbf81e005cd0d7266dd10a1bd5a
bguilas <bryanjguilas@gmail.com>
2020-11-29 11:33:16 -0800

    added ID category

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -9,7 +9,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-
 const useStyles = makeStyles({
     table: {
         minWidth: 650,
@@ -20,23 +19,23 @@ export default function Employee(props) {
     const classes = useStyles();
 
     return (
-        <div>
-            <TableContainer component={Paper}>
+        <div style={{marginLeft: 200, marginRight: 200}}>
+            <TableContainer style={{marginTop: 20, marginBottom: 20}} component={Paper}>
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Last Name</TableCell>
-                            <TableCell align="right">First Name</TableCell>
-                            <TableCell align="right">Genre</TableCell>
-                            <TableCell align="right">Email</TableCell>
+                            <TableCell style={{fontSize:'18px', fontWeight:'bold'}}>ID</TableCell>
+                            <TableCell align="right"  style={{fontSize:'18px', fontWeight:'bold'}}>Last Name</TableCell>
+                            <TableCell align="right" style={{fontSize:'18px', fontWeight:'bold'}}>First Name</TableCell>
+                            <TableCell align="right" style={{fontSize:'18px', fontWeight:'bold'}}>Genre</TableCell>
+                            <TableCell align="right" style={{fontSize:'18px', fontWeight:'bold'}}>Email</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {props.employeeList.map((result) => (
                             <TableRow key={result.id}>
-                                <TableCell component="th" scope="row">
-                                    {result.lastName}
-                                </TableCell>
+                                <TableCell component="th" scope="row">{result.id}</TableCell>
+                                <TableCell align="right" >{result.lastName}</TableCell>
                                 <TableCell align="right">{result.firstName}</TableCell>
                                 <TableCell align="right">{result.genre}</TableCell>
                                 <TableCell align="right">{result.email}</TableCell>
